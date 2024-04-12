9e67cd7e1980a6ad6a3755b223c92778831fddb1
bezbrain <belloquadriolawale@gmail.com>
2024-04-12 13:32:42 +0100

    Add background color to the screen

--- App.tsx
+++ App.tsx
@@ -1,12 +1,19 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, View } from "react-native";
 import StartGameScreens from "./screens/startGameScreens";
 
 const App = () => {
-  return <StartGameScreens />;
+  return (
+    <View style={styles.rootScreen}>
+      <StartGameScreens />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-  //
+  rootScreen: {
+    flex: 1,
+    backgroundColor: "#ddb52f",
+  },
 });
 
 export default App;
