6413ba71e48b6d157e236e4f83dc1fe927e3a22d
bezbrain <belloquadriolawale@gmail.com>
2024-04-12 13:57:02 +0100

    Add image background

--- App.tsx
+++ App.tsx
@@ -1,11 +1,18 @@
-import { StyleSheet } from "react-native";
+import { ImageBackground, StyleSheet } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 import StartGameScreens from "./screens/startGameScreens";
 
 const App = () => {
   return (
     <LinearGradient colors={["#ddb52f", "#53253c"]} style={styles.rootScreen}>
-      <StartGameScreens />
+      <ImageBackground
+        source={require("./assets/images/number-game.jpg")}
+        resizeMode="cover"
+        style={styles.rootScreen}
+        imageStyle={styles.bgImage}
+      >
+        <StartGameScreens />
+      </ImageBackground>
     </LinearGradient>
   );
 };
@@ -13,7 +20,9 @@ const App = () => {
 const styles = StyleSheet.create({
   rootScreen: {
     flex: 1,
-    // backgroundColor: "#ddb52f",
+  },
+  bgImage: {
+    opacity: 0.15,
   },
 });
 
