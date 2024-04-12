bf56140b43526fcb220357c525331e8b7447c48a
bezbrain <belloquadriolawale@gmail.com>
2024-04-12 09:36:06 +0100

    Set up the button component and other components

---
+++ screens/startGameScreens.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { View, Text, TextInput } from "react-native";
+
+const StartGameScreens = () => {
+  return (
+    <View>
+      <TextInput />
+      <Text>Start Game</Text>
+    </View>
+  );
+};
+
+export default StartGameScreens;
